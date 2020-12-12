c54c405bcc1e18bcd0d84c11868326bc9394c2d3
wslmacieiradev <wslmacieira@outlook.com>
2020-12-12 02:56:49 -0300

    cria primeira migration

--- Domain/Cliente.cs
+++ Domain/Cliente.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Curso.Domain
 {
+    [Table("Clientes")]
     public class Cliente
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Nome { get; set; }
+        [Column("Phone")]
         public string Telefone { get; set; }
         public string CEP { get; set; }
         public string Estado { get; set; }
