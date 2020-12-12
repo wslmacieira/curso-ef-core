f8cf0aa6c89ebc70030befbc9d0afc6359be0297
wslmacieiradev <wslmacieira@outlook.com>
2020-12-12 15:04:34 -0300

    inserindo registros em massa

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using Curso.Domain;
+using Curso.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 
 namespace Curso
@@ -8,15 +10,87 @@ namespace Curso
     {
         static void Main(string[] args)
         {
-            using var db = new Data.ApplicationContext();
+            /* using var db = new Data.ApplicationContext();
 
-            // db.Database.Migrate();
-            var existe = db.Database.GetPendingMigrations().Any();
-            if (existe)
+             // db.Database.Migrate();
+             var existe = db.Database.GetPendingMigrations().Any();
+             if (existe)
+             {
+                 //
+             }*/
+            // InserirDados();
+            InserirDadosEmMassa();
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
             {
-                //
-            }
-            Console.WriteLine("Hello World!");
+                Descricao = "Produto teste",
+                CodigoBarras = "123456789",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaVenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "Wagner Santos Lima",
+                CEP = "99999999",
+                Cidade = "São Paulo",
+                Estado = "SP",
+                Telefone = "99002200331"
+            };
+
+            var listaClientes = new[]
+            {
+                new Cliente
+            {
+                Nome = "Wagner Santos Lima",
+                CEP = "999999999",
+                Cidade = "São Paulo",
+                Estado = "SP",
+                Telefone = "990022003311"
+            },
+                new Cliente
+            {
+                Nome = "Daniela Santos Lima",
+                CEP = "999999999",
+                Cidade = "São Paulo",
+                Estado = "SP",
+                Telefone = "990022003311"
+            },
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.AddRange(produto, cliente);
+            // db.Set<Cliente>().AddRange(listaClientes);
+            // db.Clientes.AddRange(listaClientes);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total Registro(s): {registros}");
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto teste",
+                CodigoBarras = "123456789",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaVenda,
+                Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+            // db.Produtos.Add(produto);
+            // db.Set<Produto>().Add(produto);
+            // db.Entry(produto).State = EntityState.Added;
+            db.Add(produto);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total Registro(s): {registros}");
+
         }
     }
 }
